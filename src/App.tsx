8e03bc6ffe11a86d15ec201173e8f9e46917ee69
이원민 <wmlee@smilegate.com>
2019-09-27 18:22:38 +0900

    update: firebase 적용

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,19 @@ import styled from 'styled-components'
 
 import Container from '@/components/Container'
 import Button from '@/components/Button'
-import { useContentful } from '@/context/contentful'
+import { db } from '@/services/firebase'
 
 function App(): ReactElement {
   const [isContainer, toggleContainer] = useState(false)
   const [games, setGames] = useState([])
-  const client = useContentful()
   const handleClick = () => {
     toggleContainer(!isContainer)
   }
   useEffect(() => {
-    client
-      .getEntries({
-        content_type: 'game'
-      })
-      .then(entries => {
-        setGames(entries.items)
-      })
+    (async () => {
+      const snapshot = await db().collection('games').get()
+      console.log(snapshot.docs)
+    })()
   }, [])
   return (
     <>
---
+++ src/services/firebase.ts
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const config ={
+  apiKey: 'AIzaSyAq3iddFYAd_QzHpgu1J9v4RPTKJEQEtIU',
+  projectId: 'helminth-fd0cf',
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config)
+}
+
+export function db() {
+  return firebase.firestore()
+}
